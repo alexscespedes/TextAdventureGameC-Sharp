ff1ced050d4e453f9acb15df398cbd31c5f09c39
alexscespedes <alexandersenc99@gmail.com>
2025-06-05 19:12:42 -0400

    Connection/Exit Methods Implemented for the Room Class

--- Models/Room.cs
+++ Models/Room.cs
@@ -15,7 +15,7 @@ public class Room
 
     public List<NPC> NPCs { get; set; }
 
-    public Room (string name, string description)
+    public Room(string name, string description)
     {
         Id = Interlocked.Increment(ref nextId);
         Name = name;
@@ -24,13 +24,43 @@ public class Room
         Items = new List<Item>();
         NPCs = new List<NPC>();
     }
-    
-    public void AddExit() { }
+
+    public void AddExit(string direction, Room room)
+    {
+        Exits.Add(direction, room);
+    }
+
+    public void RemoveExit(string direction)
+    {
+        Exits.Remove(direction);
+    }
+
+    public Room? GetExit(string direction)
+    {
+        return Exits.ContainsKey(direction) ? Exits[direction] : null;
+    }
+
+    public void GetAvailableExits()
+    {
+        foreach (var e in Exits)
+        {
+            Console.WriteLine($"Direction: {e.Key}, Room: {e.Value.Name}");
+        }
+    }
+
+    public void HasExit(string direction)
+    {
+        if (!Exits.ContainsKey(direction))
+        {
+            Console.WriteLine($"The direction: {direction} is not valid");
+            return;
+        }
+        Console.WriteLine($"Direction: {direction} exits!");
+    }
 
     public void RemoveItem() { }
 
     public void AddItem() { }
 
-    public void GetAvailableExits() { }
 
 }
--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        var item1 = new Item("Ruby", "Red Stone", true, false);
+        var ball = new Item("Ball", "Basketball Item", true, false);
+        var shoes = new Item("Shoes", "Nike Shoes", true, false);
+        var lambo = new Item("Lamboghini", "The Fancy Car", true, true);
 
-        var room1 = new Room("Room 1", "Room for player 1");
+        var room1 = new Room("The 2K Apartment", "The New York Apartment");
+        var room2 = new Room("The Basket Practice", "Team Court");
+        var room3 = new Room("The Lifting Area", "Team Gym Apartment");
+        var room4 = new Room("TD Garden", "The Team Basketball Court");
+        var room5 = new Room("Press Conference", "Room with reporters");
 
-        var player1 = new Player("Alex", room1);
-
-        player1.AddToInventory(item1);
-        player1.HasItem("Ru");
-        player1.ShowInventory();
-        player1.RemoveFromInventory(item1);
-        player1.ShowInventory();
-
+        var player1 = new Player("Alexander Sencion", room1);
+        var player2 = new Player("Stefan Salvatore", room5);
 
 
     }
